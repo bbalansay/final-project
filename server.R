276546f2eb518885cfd20a7c3489e58f88b161eb
McKinley Harvey <mack.kinley@gmail.com>
2018-03-05 18:44:09 -0800

    Additional edits

--- server.R
+++ server.R
@@ -1,6 +1,28 @@
 library(shiny)
 
 server <- function(input, output) {
+  output$yearPlot <- renderPlot({
+    titles <- paste("Number of Deaths By Age in", input$year)
+    drug.deaths <- read.csv('data/Accidental_Drug_Related_Deaths__2012-June_2017-2.csv', 
+                            stringsAsFactors = FALSE) %>% 
+      filter(Race != "", Date != "", Age != "", Sex != "", Location != "")
+    year.deaths <- drug.deaths[substr(drug.deaths$Date, 7, 10) == input$year, ] 
+    if(input$fill == "Sex") {
+      ggplot(data = year.deaths) +
+        geom_bar(mapping = aes(x = Age, fill = Sex)) +
+        labs(title = paste(titles, "When Compared with", input$fill))
+    } else if (input$fill == "Location") {
+      ggplot(data = year.deaths) +
+        geom_bar(mapping = aes(x = Age, fill = Location)) +
+        labs(title = paste(titles, "When Compared with", input$fill))
+    } else {
+      ggplot(data = year.deaths) +
+        geom_bar(mapping = aes(x = Age)) +
+        labs(title = titles)
+        
+    }
+    
+  })
   
 }
 
--- ui.R
+++ ui.R
@@ -48,7 +48,19 @@ ui <- fluidPage(
     ##################################################################
     conditionalPanel(
       condition = "input.dataTabs == 'Effects'",
-      h3('Effects')
+      h3('Effects'), 
+      sliderInput("year",
+                  "Select A Year",
+                  min = 2012,
+                  max = 2017,
+                  value = 2012,
+                  sep = ""),
+      
+      selectInput("fill",
+                  "What Would You Like to Compare Within The Charts?",
+                  c("Sex",
+                    "Location of Death" = "Location",
+                    "Nothing"))
     )
     
   ),
@@ -82,7 +94,9 @@ ui <- fluidPage(
                 ##############################
                 tabPanel("Effects", 
                          h3("Effects"),
-                         br(), p("Effects"))
+                         br(), p("Effects"),
+                plotOutput("yearPlot")
+                )
     )
   )
 )
