dad2fd26cdc19115381719c12a57dccac0d64a71
Bradley Balansay <bradleybalansay@gmail.com>
2018-03-07 17:23:56 -0800

    Colored shit

--- server.R
+++ server.R
@@ -35,62 +35,51 @@ server <- function(input, output) {
   })
   output$weedTime <-renderPlot({
     
-    
     drug.use <- paste0(input$drug_choice, ".use")
     
     drug.data.filtered <- select(drug.data, age, drug.use) %>%
       filter(age >= input$min.age) %>%
       filter(age <= input$max.age)
     
-    geom_bar(stat="identity", aes(fill = drugs))
     if(input$drug_choice == "alcohol") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=alcohol.use)) +
-        geom_bar(stat="identity")
+        geom_bar(stat="identity", aes(fill = age))
     } else if (input$drug_choice == "marijuana") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=marijuana.use)) +
-        geom_bar(stat="identity")
-    }else if (input$drug_choice == "cocaine") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "cocaine") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=cocaine.use)) +
-        geom_bar(stat="identity")
-    }  else if (input$drug_choice == "crack") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "crack") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=crack.use)) +
-        geom_bar(stat="identity")
-    }
-    
-    else if (input$drug_choice == "heroin") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "heroin") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=heroin.use)) +
-        geom_bar(stat="identity")
-    }
-    
-    else if (input$drug_choice == "hallucinogen") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "hallucinogen") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=hallucinogen.use)) +
-        geom_bar(stat="identity")
-    }
-    
-    else if (input$drug_choice == "inhalant") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "inhalant") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=inhalant.use)) +
-        geom_bar(stat="identity")
-    }
-    else if (input$drug_choice == "pain.reliever") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "pain.reliever") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=pain.reliever.use)) +
-        geom_bar(stat="identity")
-    }
-    else if (input$drug_choice == "oxycontin") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "oxycontin") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=oxycontin.use)) +
-        geom_bar(stat="identity")
-    }
-    else if (input$drug_choice == "tranquilizer") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "tranquilizer") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=tranquilizer.use)) +
-        geom_bar(stat="identity")
-    }  else if (input$drug_choice == "stimulant") {
+        geom_bar(stat="identity", aes(fill = age))
+    } else if (input$drug_choice == "stimulant") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=stimulant.use)) +
-        geom_bar(stat="identity")
+        geom_bar(stat="identity", aes(fill = age))
     } else if (input$drug_choice == "meth") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=meth.use)) +
-        geom_bar(stat="identity")
+        geom_bar(stat="identity", aes(fill = age))
     } else if (input$drug_choice == "sedative") {
       age.graph <- ggplot(data=drug.data.filtered, aes(x=age, y=sedative.use)) +
-        geom_bar(stat="identity")
+        geom_bar(stat="identity", aes(fill = age))
     }
     plot(age.graph)
     return(age.graph)
